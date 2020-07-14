4302608cff093682bc6e24227a4c1e3d8c9668c1
Nesta Maulana <nestamaulana165@gmail.com>
2020-07-14 16:28:25 +0700

    tamabhan

--- app/Http/Controllers/Transaction/Rollie/RPRController.php
+++ app/Http/Controllers/Transaction/Rollie/RPRController.php
@@ -5,10 +5,59 @@ namespace App\Http\Controllers\Transaction\Rollie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Transaction\Rollie\Palet;
+Use App\Models\Transaction\Rollie\WoNumber;
+use App\Models\Master\Product;
+
+use App\Models\Transaction\Rollie\CppHead;
+use App\Models\Transaction\Rollie\CppDetail;
+
 class RPRController extends Controller
 {
     public function exportBar(Request $request)
     {
-        dd($request->all());
+        $bar_number     = $request['data_bar'][1]['__EMPTY_8'];
+        // unset($request['data_bar'][0]);
+        // unset($request['data_bar'][1]);
+        // unset($request['data_bar'][2]);
+        // unset($request['data_bar'][3]);
+        // unset($request['data_bar'][4]);
+        // unset($request['data_bar'][5]);
+        // unset($request['data_bar'][6]);
+        // unset($request['data_bar'][7]);
+        $array_product   = array();
+        
+        foreach ($request['data_bar'] as $key => $value) 
+        {
+            if ($key > 6) 
+            {
+                $product['oracle_code']         = $value['__EMPTY_1'];
+                $product['product_name']        = $value['__EMPTY_2'];
+                $product['lot_number']          = $value['__EMPTY_4'];
+                $product['bar_status']          = $value['__EMPTY_5'];
+                $product['bar_date']            = $value['__EMPTY_6'];
+                array_push($array_product,$product);
+            }
+        }
+        $fail_update                = '';
+        foreach ($array_product as $list_lot) 
+        {
+            $explode_lot            = explode('-',$list_lot['lot_number']);
+            $lot_numbers            = CppDetail::where('lot_number',$explode_lot[0])->first();
+            $palets                 = $lot_numbers->palets->where('palet',$explode_lot[1]);
+            if (is_null($palets->bar_number))
+            {
+                $palets->bar_number     = $bar_number;
+            }
+            /*else
+            {
+                $fail_update        .= ' Palet '.$palets->palet.' ';
+            } */
+            $palets->bar_status     = $list_lot['bar_status'];
+            $palets->bar_date       = $list_lot['bar_date'];
+            /* $palets->bar_note       = $list_lot['bar_note']; */
+            $palets->save();
+        }
+        return ['success'=>true,'message'=>'Data berhasil diupload'];
     }
 }
--- database/migrations/2020_02_10_203254_create_palets_table.php
+++ database/migrations/2020_02_10_203254_create_palets_table.php
@@ -17,12 +17,19 @@ class CreatePaletsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('cpp_detail_id')->comment('connected to cpp detail tabel');
             $table->char('palet',10);
+            
             $table->dateTime('start');
             $table->dateTime('end')->nullable();
+            
             $table->smallInteger('jumlah_box')->nullable();
             $table->smallInteger('jumlah_pack')->nullable();
+            
             $table->enum('analisa_mikro_30_status', ['0', '1', '2', '3'])->nullable();
             $table->enum('analisa_mikro_55_status', ['0', '1', '2', '3'])->nullable();
+            $table->char('bar_number',30)->nullable();
+            $table->char('bar_status',5)->nullable();
+            $table->text('bar_note')->nullable();
+            
             $table->bigInteger('created_by')->comment('connected to user table');
             $table->bigInteger('updated_by')->comment('connected to user table')->nullable();
             $table->bigInteger('deleted_by')->comment('connected to user table')->nullable();
